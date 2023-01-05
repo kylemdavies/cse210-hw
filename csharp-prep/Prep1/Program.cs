e3f3307daba71e503a565a16e2316f12510234f5
kylemdavies <dav19010@byui.edu>
2023-01-05 15:18:58 -0800

    Commit

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        String FName = "";
+        String LName = "";
+        Console.WriteLine("What is your First Name?  ");
+        FName = Console.ReadLine();
+        Console.WriteLine("What is your Last Name?  ");
+        LName = Console.ReadLine();
+        Console.WriteLine($"Your name is {LName}, {FName} {LName}");
     }
 }
