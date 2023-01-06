fb1f9235ea65dc2cd574215e0b87a7ed006e4f15
kylemdavies <dav19010@byui.edu>
2023-01-05 16:01:22 -0800

    commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        // Letter Grade Reveal
+
+        Console.WriteLine("What is your Percentage Grade? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letterGrade = "";
+        if (number >= 90) {
+            letterGrade = "A";
+        }
+        else if (number >= 80) {
+            letterGrade = "B";
+        }
+        else if (number >= 70) {
+            letterGrade = "C";
+        }
+        else if (number >= 60) {
+            letterGrade = "D";
+        }
+        else {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your Grade is {letterGrade}.");
+        
+        //Pass or Fail Message 
+
+        if (number >= 70) {
+            Console.WriteLine($"Congrats! You passed!");
+        }
+        else {
+            Console.WriteLine($"Darn! Try again next time!");
+        }
     }
 }
