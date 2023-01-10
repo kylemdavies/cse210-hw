5de113fe95743f92f8b78d2e382139bea08bf778
kylemdavies <dav19010@byui.edu>
2023-01-10 10:18:43 -0800

    commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string keepGoing = "Y";
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,10);
+        int guess = 0;
+
+        do
+        {
+            if (keepGoing == "Y")
+            {
+                while (guess != number)
+                {
+                    Console.Write("Guess a Number? ");
+                    string userGuess = Console.ReadLine();
+                    guess = int.Parse(userGuess);
+                    if (guess < number)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (guess > number)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You Got It!");
+                    }
+                }
+            }
+            Console.Write("Do you want to continue? (Y/N) ");
+            keepGoing = Console.ReadLine();
+            keepGoing = keepGoing.ToUpper();
+        } while (keepGoing == "Y");
     }
 }
