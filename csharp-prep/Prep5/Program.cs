ca9ecf68c9171ef1f1ec6b714a98294c65f189a9
kylemdavies <dav19010@byui.edu>
2023-01-10 11:48:52 -0800

    commit

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = userName();
+        float number = userNumber();
+        float square = squareNumber(number);
+
+        displayResult(name, square);
+    }
+
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+    static string userName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static float userNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        float number = float.Parse(Console.ReadLine());
+
+        return number;
+    }
+    static float squareNumber(float number)
+    {
+        float square = number * number;
+
+        return square;
+    }
+    static void displayResult(string name, float square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
