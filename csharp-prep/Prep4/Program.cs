00aaf388985068addf512610e858ed9af05b51ab
kylemdavies <dav19010@byui.edu>
2023-01-10 11:24:21 -0800

    commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        float sum = 0;
+        float average = 0;
+        float bigNum = 0;
+        List<float> numbers = new List<float>();
+        Console.WriteLine(" Enter a list of numbers, type 0 when finished.");
+        Console.WriteLine(" Enter number: ");
+        float input = float.Parse(Console.ReadLine());
+        while (input != 0)
+        {
+            numbers.Add(input);
+            Console.WriteLine(" Enter number: ");
+            input = float.Parse(Console.ReadLine());
+        }
+        
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > bigNum)
+            {
+                bigNum = number;
+            }
+
+        }
+        average = sum / numbers.Count;
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {bigNum}");
+        
     }
 }
