0a718ca5ff8b28f7d86c6b05f4371e46ede17369
kylemdavies <dav19010@byui.edu>
2023-01-24 13:11:28 -0800

    commit

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -3,7 +3,7 @@ using System;
 public class Resume
 {
     public string _name = "";
-    public List<Job> = new List<Job>();
+    public List<Job> _jobs = new List<Job>();
     public void Display()
     {
         Console.WriteLine($"Name: {_name}");
