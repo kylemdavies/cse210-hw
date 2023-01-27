5c9d2ef227d1a9adad40e67072c20b8da5d633e0
kylemdavies <dav19010@byui.edu>
2023-01-27 14:33:48 -0800

    commit

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,7 +1,7 @@
 using System;
 public class PromptGenerator
 {
-    static string[] prompts = {"What was your favorite thing you did today?", "What is something you learned today?", "How do you feel about today? Why?"};
+    static string[] prompts = {"What was your favorite thing you did today?", "What is something you learned today?", "How do you feel about today? Why?", "Who was the most interesting person I interacted with today?", "What was the best part of my day?", "How did I see the hand of the Lord in my life today?", "What was the strongest emotion I felt today?", "If I had one thing I could do over today, what would it be?"};
     static public List<string> _promptsList = new List<string>(prompts);
     static int promptsCount = _promptsList.Count;
     static Random rnd = new Random();
