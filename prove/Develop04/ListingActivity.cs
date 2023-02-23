24a7992f1e2bc0d925a2766d0368231abe0bc06d
kylemdavies <dav19010@byui.edu>
2023-02-23 10:57:23 -0800

    commit

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class ListingActivity : Activity
+{
+    public ListingActivity(string activityName, string description) : base(activityName, description)
+        {
+            
+        }
+}
