1bd6773e79c809b84fc437f5b13660dfe28bb952
kylemdavies <dav19010@byui.edu>
2023-03-24 12:20:02 -0700

    commit

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -9,11 +9,11 @@ class Program
         List<string> stringPersonas = new List<string>();
         List<string> stories = new List<string>();
         string Menu = "Menu Options \n1. Characters \n2. Stories \n3. Save \n4. Load \n5. Quit";
-        while (Choice != 6)
+        while (Choice != 5)
         {
             Console.WriteLine($"{Menu}");
             Console.WriteLine($"v Select a choice from the menu: ");
-            Choice = int.Parse(Console.ReadLine()); 
+            Choice = int.Parse(Console.ReadLine());
             if (Choice == 1)
             {
                 Console.WriteLine("1. New Random Character \n2. New Custom Character \n3. List Characters");
@@ -21,17 +21,17 @@ class Program
                 if (Choice == 1)
                 {
                     //Will create a random "Character" (Persona) and then add it to the personas List
-                    Console.Clear();
+                    Console.WriteLine("You successfully created a random Character!");
                 }
                 else if (Choice == 2)
                 {
                     //Will display questions and give options for each attribute and create a cutomized "Character" (Persona) and then add it to the personas List
-                    Console.Clear();
+                    Console.WriteLine("You successfully created a Character!");
                 }
                 else
                 {
                     //Will display the personas List
-                    Console.Clear();
+                    Console.WriteLine("Here is your list of Characters!");
                 }
             }
             else if (Choice == 2)
@@ -41,17 +41,17 @@ class Program
                 if (Choice == 1)
                 {
                     //Will create a random story and then add it to the stories List
-                    Console.Clear();
+                    Console.WriteLine("You successfully created a random Story!");
                 }
                 else if (Choice == 2)
                 {
                     //Will display questions like "What is the name of the city?" or "What is the bad guy after?" and then put the answers together in a "Madlibs" style story and then add it to the stories List
-                    Console.Clear();
+                    Console.WriteLine("You successfully created a Story!");
                 }
                 else
                 {
                     //Will display the stories List
-                    Console.Clear();
+                    Console.WriteLine("Here are your stories!");
                 }
             }
             else if (Choice == 3)
@@ -61,12 +61,12 @@ class Program
                 if (Choice == 1)
                 {
                     // Will ask what they want the file to be called then saves the personas list to the file
-                    Console.Clear();
+                    Console.WriteLine("You successfully saved your characters!");
                 }
                 else
                 {
                     // Will ask what they want the file to be called then saves the stories list to the file
-                    Console.Clear();
+                    Console.WriteLine("You successfully saved your stories!");
                 }
             }
             else if (Choice == 4)
@@ -76,12 +76,12 @@ class Program
                 if (Choice == 1)
                 {
                     // Will ask what the name of the file is then loads the personas list from the file
-                    Console.Clear();
+                    Console.WriteLine("You successfully loaded your characters!");
                 }
                 else
                 {
                     // Will ask what the name of the file is then loads the stories list from the file
-                    Console.Clear();
+                    Console.WriteLine("You successfully loaded your stories!");
                 }
             }
             else
